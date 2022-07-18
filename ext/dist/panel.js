bef9bb88fef9c1cf51e52a8e09eec238f9b31fe7
Vyrtsev Mikhail <reeywhaar@gmail.com>
2022-07-18 18:41:23 +0300

    Show current tab index

--- ext/dist/panel.js
+++ ext/dist/panel.js
@@ -1,13 +1,17 @@
 console.log("TABS START");
 async function main() {
-  console.log("TABS ADD LISTENER");
-  browser.tabs.onUpdated.addListener(async () => {
-    console.log("TABS UPDATED");
-  });
-  console.log("TABS ADDED LISTENER");
-
   const first = document.getElementById("first");
   const second = document.getElementById("second");
+  const current = document.getElementById("current");
+
+  console.log("TABS ADD LISTENER");
+
+  browser.tabs.onUpdated.addListener(async () => {
+    console.log("TABS UPDATED");
+    const tabs = await browser.tabs.query({ currentWindow: true });
+    current.innerText = tabs.findIndex((t) => !!t.active);
+  });
+  console.log("TABS ADDED LISTENER");
 
   first.addEventListener("click", async () => {
     const tabs = await browser.tabs.query({ currentWindow: true });
